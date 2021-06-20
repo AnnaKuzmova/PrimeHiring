f2dea184939f8d39f0b85e1e41ef4b87c8442d13
AnnaKuzmova <anni.kuzmovastd@gmail.com>
2021-06-20 15:23:09 +0300

    Starting Point
    Basic set up made - creating index.html page, setting up css folder and
    styles inside it. Created folder to store related images to the task.

---
+++ test.js
@@ -0,0 +1,21 @@
+// let persons = [
+//     {
+//         name : 'Daniel',
+//         age : 45
+//     },
+//     {
+//         name : 'Sandra',
+//         age : 34
+//     }
+// ]
+
+// let persons_serialized = JSON.stringify(persons)
+
+// window.localStorage.setItem("persons", persons_serialized)
+// console.log(localStorage)
+
+// let persons_get = JSON.parse(window.localStorage.getItem("persons"))
+// console.log(persons_get)
+
+
+
